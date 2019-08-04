684dea649f1b0bd904928fd28282ee86aad175e2
HaiBeZ <47478847+HaiBeZ@users.noreply.github.com>
2019-08-04 23:55:24 +0800

    完成了index页面的两种状态
    完成了index页面的两种状态

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,37 @@
 //index.js
-const api = require('../../utils/API.js');
+//获取应用实例
+const app = getApp()
+
 Page({
-    onLoad: function (options) {
-        api.request({
-            url: '/form',
-            method: "POST",
-            data: {
-               
-            }
-        }).then(respond => {
-            console.log(respond)
-        })
-    },
+  data: {
+    is_sign_up:1 //有无报名
+  },
+  onLoad: function () {
+  
+  },
+  to_about_us:function(){
+    wx.navigateTo({
+      url: '../about_us/about_us',
+    })
+  },
+  to_form: function () {
+    wx.navigateTo({
+      url: '../form/form',
+    })
+  },
+  to_result: function () {
+    wx.navigateTo({
+      url: '../result/result',
+    })
+  },
+  to_watch_form: function () {
+    wx.navigateTo({
+      url: '../watch_form/watch_form',
+    })
+  },
+  to_queue_info:function(){
+    wx.navigateTo({
+      url: '../queue_info/queue_info',
+    })
+  }
 })
